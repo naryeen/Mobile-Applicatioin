021c8935c320236ec2c347ea81e5000ea295af36
NarBdrKharka <narbdr81@gmail.com>
2021-03-15 22:13:44 +0600

    initial commit

---
+++ AndroidStudioProjects/TODO3/app/src/main/java/gcit/edu/bt/todo3/MainActivity.java
@@ -0,0 +1,22 @@
+package gcit.edu.bt.todo3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void activity(View view) {
+        Intent object = new Intent(this,MainActivity2.class);
+        startActivity(object);
+    }
+
+}
