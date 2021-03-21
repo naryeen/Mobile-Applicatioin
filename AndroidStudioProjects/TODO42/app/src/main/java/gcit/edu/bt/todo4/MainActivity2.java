ff28c34d6374118fef7031697cc73c9b65b14782
NarBdrKharka <narbdr81@gmail.com>
2021-03-21 22:31:11 +0600

    initial comment

---
+++ AndroidStudioProjects/TODO42/app/src/main/java/gcit/edu/bt/todo4/MainActivity2.java
@@ -0,0 +1,51 @@
+package gcit.edu.bt.todo4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    // Unique tag for the intent reply.
+    public static final String EXTRA_REPLY =
+            "gcit.edu.bt.todo4.extra.REPLY";
+
+    // EditText for the reply.
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        // Initialize view variables.
+        mReply = findViewById(R.id.editText_second);
+
+        // Get the intent that launched this activity, and the message in
+        // the intent extra.
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Put that message into the text_message TextView
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+    }
+    public void returnReply(View view) {
+        // Get the reply message from the edit text.
+        String reply = mReply.getText().toString();
+
+        // Create a new intent for the reply, add the reply message to it
+        // as an extra, set the intent result, and close the activity.
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+}
+
+
+
